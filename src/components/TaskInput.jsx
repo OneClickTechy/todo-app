5f2819a2babda516892f3c659d7a4332a2bcffc0
OneClickTechy <oneclicktechy@gmail.com>
2024-09-24 15:49:57 +0530

    focus input hook is added

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { FaTimes, FaPlus, FaSave} from "react-icons/fa";
+import React, { useRef } from "react";
+import { FaTimes, FaPlus, FaSave } from "react-icons/fa";
 
 const TaskInput = ({
   taskPriority,
@@ -17,105 +17,117 @@ const TaskInput = ({
   handleTaskEdit,
   handleCancel,
 }) => {
+  const inputRef = useRef(null);
+
+  const handleFocus = () => {
+    inputRef.current.focus();
+  };
   return (
-    
-    <form id="inputForm"
-    className="relative flex flex-col justify-center items-center gap-6 border-2 border-black rounded-3xl p-4 text-green-700 m-4 max-w-sm w-full sm:max-w-md md:max-w-lg lg:max-w-xl"
-    onSubmit={(e) => e.preventDefault()}
-  >
-    <h2 className="text-2xl font-bold underline text-green-700">
-      {!editMode ? "Add Task" : "Edit Task"}
-    </h2>
-    
-    <FaTimes 
-      className="absolute top-4 right-4 text-2xl hover:text-green-400 active:text-green-600 focus:text-green-600 cursor-pointer"
-      onClick={handleCancel}
-    />
-    
-    <label htmlFor="task" className="sr-only">
-      Task Name
-    </label>
-    <input
-      onChange={handleTaskInput}
-      type="text"
-      name="task"
-      id="task"
-      placeholder="Enter a new task"
-      className="border-2 border-black rounded-3xl h-10 px-4 focus:outline-none focus:ring focus:ring-green-400 placeholder:text-green-700/50 w-full"
-      value={taskValue}
-    />
-  
-    <label htmlFor="date" className="sr-only">
-      Due Date
-    </label>
-    <input
-      type="date"
-      name="date"
-      id="date"
-      className="border-2 border-black rounded-3xl h-10 px-4 focus:outline-none focus:ring focus:ring-green-400 text-green-700 w-full"
-      value={dateValue}
-      onChange={handleDateInput}
-    />
-  
-    <label htmlFor="priority" className="sr-only">
-      Task Priority
-    </label>
-    <select
-      name="priority"
-      id="priority"
-      className="border-2 border-black rounded-3xl h-10 px-4 focus:outline-none focus:ring focus:ring-green-400 text-green-600 w-full"
-      value={priorityValue}
-      onChange={handlePriorityInput}
+    <form
+      id="inputForm"
+      className="relative flex flex-col justify-center items-center gap-6 border-2 border-black rounded-3xl p-4 text-green-700 m-4 max-w-sm w-full sm:max-w-md md:max-w-lg lg:max-w-xl"
+      onSubmit={(e) => e.preventDefault()}
     >
-      <option value="" disabled>
-        --- Select Priority ---
-      </option>
-      {Object.entries(taskPriority).map(([key, value]) => (
-        <option key={key} value={key}>
-          {value}
-        </option>
-      ))}
-    </select>
-  
-    <label htmlFor="status" className="sr-only">
-      Task Status
-    </label>
-    <select
-      name="status"
-      id="status"
-      className="border-2 border-black rounded-3xl h-10 px-4 focus:outline-none focus:ring focus:ring-green-400 text-green-600 w-full"
-      value={statusValue}
-      onChange={handleStatusInput}
-    >
-      <option value="" disabled>
-        --- Select Status ---
-      </option>
-      {Object.entries(taskStatus).map(([key, value]) => (
-        <option key={key} value={key}>
-          {value}
-        </option>
-      ))}
-    </select>
-  
-    {editMode ? (
-      <FaSave 
-        role="button"
-        tabIndex="0"
-        aria-label="Save task"
-        className="text-2xl hover:text-green-400 active:text-green-600 focus:text-green-600 text-green-700 cursor-pointer"
-        onClick={editMode ? handleTaskEdit : handleSubmit}
+      <h2 className="text-2xl font-bold underline text-green-700">
+        {!editMode ? "Add Task" : "Edit Task"}
+      </h2>
+
+      <FaTimes
+        className="absolute top-4 right-4 text-2xl hover:text-green-400 active:text-green-600 focus:text-green-600 cursor-pointer"
+        onClick={handleCancel}
       />
-    ) : (
-      <FaPlus
-        role="button"
-        tabIndex="0"
-        aria-label="Add task"
-        className="text-2xl hover:text-green-400 active:text-green-600 focus:text-green-600 text-green-700 cursor-pointer"
-        onClick={editMode ? handleTaskEdit : handleSubmit}
+
+      <label htmlFor="task" className="sr-only">
+        Task Name
+      </label>
+      <input
+        autoFocus
+        onChange={handleTaskInput}
+        ref={inputRef}
+        type="text"
+        name="task"
+        id="task"
+        placeholder="Enter a new task"
+        className="border-2 border-black rounded-3xl h-10 px-4 focus:outline-none focus:ring focus:ring-green-400 placeholder:text-green-700/50 w-full"
+        value={taskValue}
       />
-    )}
-  </form>
-  
+
+      <label htmlFor="date" className="sr-only">
+        Due Date
+      </label>
+      <input
+        type="date"
+        name="date"
+        id="date"
+        className="border-2 border-black rounded-3xl h-10 px-4 focus:outline-none focus:ring focus:ring-green-400 text-green-700 w-full"
+        value={dateValue}
+        onChange={handleDateInput}
+      />
+
+      <label htmlFor="priority" className="sr-only">
+        Task Priority
+      </label>
+      <select
+        name="priority"
+        id="priority"
+        className="border-2 border-black rounded-3xl h-10 px-4 focus:outline-none focus:ring focus:ring-green-400 text-green-600 w-full"
+        value={priorityValue}
+        onChange={handlePriorityInput}
+      >
+        <option value="" disabled>
+          --- Select Priority ---
+        </option>
+        {Object.entries(taskPriority).map(([key, value]) => (
+          <option key={key} value={key}>
+            {value}
+          </option>
+        ))}
+      </select>
+
+      <label htmlFor="status" className="sr-only">
+        Task Status
+      </label>
+      <select
+        name="status"
+        id="status"
+        className="border-2 border-black rounded-3xl h-10 px-4 focus:outline-none focus:ring focus:ring-green-400 text-green-600 w-full"
+        value={statusValue}
+        onChange={handleStatusInput}
+      >
+        <option value="" disabled>
+          --- Select Status ---
+        </option>
+        {Object.entries(taskStatus).map(([key, value]) => (
+          <option key={key} value={key}>
+            {value}
+          </option>
+        ))}
+      </select>
+      <button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          editMode ? handleTaskEdit() : handleSubmit();
+          handleFocus();
+        }}
+      >
+        {editMode ? (
+          <FaSave
+            role="button"
+            tabIndex="0"
+            aria-label="Save task"
+            className="text-2xl hover:text-green-400 active:text-green-600 focus:text-green-600 text-green-700 cursor-pointer"
+          />
+        ) : (
+          <FaPlus
+            role="button"
+            tabIndex="0"
+            aria-label="Add task"
+            className="text-2xl hover:text-green-400 active:text-green-600 focus:text-green-600 text-green-700 cursor-pointer"
+          />
+        )}
+      </button>
+    </form>
   );
 };
 
